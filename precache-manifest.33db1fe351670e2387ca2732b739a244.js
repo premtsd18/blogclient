deb0269d7af5a39f260774f70cfa472b6c89f009
Prem <premtsd@gmail.comgit config --global user.email premtsd@gmail.com>
2020-08-13 20:25:34 +0530

    Updates

---
+++ precache-manifest.33db1fe351670e2387ca2732b739a244.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea69e07a9e7985840bc20a6a239cc671",
+    "url": "/blogclient/index.html"
+  },
+  {
+    "revision": "cb8b8147434b9e930f05",
+    "url": "/blogclient/static/css/main.70b81534.chunk.css"
+  },
+  {
+    "revision": "d87ff34f019b043f70ad",
+    "url": "/blogclient/static/js/2.51aca91b.chunk.js"
+  },
+  {
+    "revision": "8ebb70227fec3b61ad3af3d99ebe8940",
+    "url": "/blogclient/static/js/2.51aca91b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb8b8147434b9e930f05",
+    "url": "/blogclient/static/js/main.1a3d3467.chunk.js"
+  },
+  {
+    "revision": "4617ffd48335909428cf",
+    "url": "/blogclient/static/js/runtime-main.975fd174.js"
+  }
+]);
